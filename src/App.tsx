b07549775acc4e72db3e89930d58be9b7a2a93bb
natanpms <natanportoms@outlook.com>
2025-05-15 20:23:27 -0300

    feat: iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='flex justify-center items-center min-h-screen'>
+        <h1 className='underline font-bold text-3xl text-amber-300'>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
