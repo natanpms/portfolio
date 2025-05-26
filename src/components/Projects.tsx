4dba79700d609304559087a1ee281a8470bda3ab
natanpms <natanportoms@outlook.com>
2025-05-26 20:44:12 -0300

    fix: icone do badge da seção de projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -94,11 +94,84 @@ export default function Projects() {
                                     src={projectPic}
                                     alt="project"
                                 />
-                                <div className='flex justify-around items-center gap-7 mb-2'>
-                                    <h3 className="text-regular font-bold ">{project.name}</h3>
-                                    <div className={`badge badge-outline badge-sm ${project.ies_finally ? "badge-success" : "badge-warning"}`}>
-                                        <svg className="size-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="currentColor" strokeLinejoin="miter" strokeLinecap="butt"><circle cx="12" cy="12" r="10" fill="none" stroke="currentColor" strokeLinecap="square" stroke-miterlimit="10" strokeWidth="2"></circle><polyline points="7 13 10 16 17 8" fill="none" stroke="currentColor" strokeLinecap="square" stroke-miterlimit="10" strokeWidth="2"></polyline></g></svg>
-                                        {project.ies_finally ? "Finalizado" : "Em andamento"}
+                                <div className="flex justify-around items-center gap-7 mb-2">
+                                    <h3 className="text-regular font-bold">{project.name}</h3>
+
+                                    <div
+                                        className={`badge badge-outline badge-sm ${project.ies_finally ? "badge-success" : "badge-warning"} flex items-center gap-1`}
+                                    >
+                                        {project.ies_finally ? (
+                                            <>
+                                                <svg
+                                                    className="size-[1em]"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <g
+                                                        fill="currentColor"
+                                                        strokeLinejoin="miter"
+                                                        strokeLinecap="butt"
+                                                    >
+                                                        <circle
+                                                            cx="12"
+                                                            cy="12"
+                                                            r="10"
+                                                            fill="none"
+                                                            stroke="currentColor"
+                                                            strokeLinecap="square"
+                                                            strokeMiterlimit="10"
+                                                            strokeWidth="2"
+                                                        />
+                                                        <polyline
+                                                            points="7 13 10 16 17 8"
+                                                            fill="none"
+                                                            stroke="currentColor"
+                                                            strokeLinecap="square"
+                                                            strokeMiterlimit="10"
+                                                            strokeWidth="2"
+                                                        />
+                                                    </g>
+                                                </svg>
+                                                Finalizado
+                                            </>
+                                        ) : (
+                                            <>
+                                                <svg
+                                                    className="size-[1em]"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 18 18"
+                                                >
+                                                    <g fill="currentColor">
+                                                        <path
+                                                            d="M7.638,3.495L2.213,12.891c-.605,1.048,.151,2.359,1.362,2.359H14.425c1.211,0,1.967-1.31,1.362-2.359L10.362,3.495c-.605-1.048-2.119-1.048-2.724,0Z"
+                                                            fill="none"
+                                                            stroke="currentColor"
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            strokeWidth="1.5"
+                                                        />
+                                                        <line
+                                                            x1="9"
+                                                            y1="6.5"
+                                                            x2="9"
+                                                            y2="10"
+                                                            fill="none"
+                                                            stroke="currentColor"
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            strokeWidth="1.5"
+                                                        />
+                                                        <path
+                                                            d="M9,13.569c-.552,0-1-.449-1-1s.448-1,1-1,1,.449,1,1-.448,1-1,1Z"
+                                                            fill="currentColor"
+                                                            data-stroke="none"
+                                                            stroke="none"
+                                                        />
+                                                    </g>
+                                                </svg>
+                                                Em andamento
+                                            </>
+                                        )}
                                     </div>
                                 </div>
                                 <p className="text-sm">{project.description}</p>
